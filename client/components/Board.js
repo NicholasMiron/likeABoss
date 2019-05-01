f9e8aebc6e71ba30cdf4bec1ab44ece5998c3afa
Nicholas Miron <nicholasmiron@gmail.com>
2019-05-01 11:17:32 -0500

    Added the ability to add new Lists

--- client/components/Board.js
+++ client/components/Board.js
@@ -5,15 +5,60 @@ class Board extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lists: [[{description:'hello', person:'nick'}],[{description:'hello', person:'nick'}],[{description:'hello', person:'nick'}],[{description:'hello', person:'nick'}],[{description:'hello', person:'nick'}]],
+      lists: [{title:'List1',cards:[{title:'Hello',description:'World',people:['nick', 'joe', 'sally']}]},{title:'List1',cards:[{title:'Hello',description:'World',people:['nick', 'joe', 'sally']}]},{title:'List1',cards:[{title:'Hello',description:'World',people:['nick', 'joe', 'sally']}]}]
     }
   }
+
+  // let listStructure = {
+  //   title: 'String',
+  //   cards: [
+  //     {
+  //       title: 'String',
+  //       description: 'String',
+  //       people: 'Array',
+  //       labels: 'Array',
+  //       dueBy: 'String',
+  //     }
+  //   ]
+  // }
+
+  // let lists = [
+  //   listStrucutre,
+  //   listStrucutre,
+  //   listStrucutre
+  // ]
+
+  addCard(listId, card) {
+    console.log('Add a card');
+    // let updatedList = [this.state.lists[listId]]
+  }
+
+  addList(e) {
+    if (e.keyCode === 13) {
+      if (e.target.value !== '') {
+        let newLists = this.state.lists;
+        let newList = {title: e.target.value, cards: []}
+        newLists.push(newList);
+        this.setState({lists: newLists});
+      }
+    }
+  }
+
   render() { 
     return ( 
       <div className={'board'}>
         {this.state.lists.map((list, i) => (
-          <List key={i} list={this.state.lists[i]} />
+          <List key={i} list={this.state.lists[i]} addCard={this.addCard.bind(this)} />
         ))}
+        <div className={'newListRow'}>
+          <input 
+            type='text' 
+            className={'newList'} 
+            placeholder='Give me a name...'
+            onKeyDown={e => this.addList(e)}
+          >
+          </input>
+        </div>
       </div>
     );
   }
--- client/components/List.js
+++ client/components/List.js
@@ -2,18 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Card from './Card';
 
-const List = ({list}) => {
+const List = ({list, addCard}) => {
   return ( 
     <div className={'list'}>
-      {list.map((card, i) => (
+      <div className={'listTitle'}>{list.title}</div>
+      {list.cards.map((card, i) => (
         <Card key={i} card={card} />
       ))}
+      <button className={'newCard'} onClick={() => addCard()}>New Card</button>
     </div>
    );
 }
 
 List.propTypes = {
   list: PropTypes.array,
+  addCard: PropTypes.func,
 }
 
 export default List;
